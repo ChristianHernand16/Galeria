e26cb06c14eaf4b74c7c42995b99b86d3d182b2a
ChristianHernand16 <christianh1996@hotmail.com>
2019-05-21 16:11:38 -0500

    final

--- galeria.js
+++ galeria.js
@@ -1,36 +1,37 @@
-var n = 1;
-function adelante() {
-    n++;
-    if (n > 10)
-        n = 1;
-    var foto = document.getElementById("ia");
-    foto.src = n + ".jpg";
-}
-function atras() {
-    n--;
-    if (n < 1)
-        n = 10-5;
-    var foto = document.getElementById("ia");
-    foto.src = n + ".jpg";
+var lista5 = new Array();
+
+var i = 0;
+aletorio();
+
+function siguiente() {
+    i > 4 ? (i = 0) : (i = i);
+    var img = document.getElementById("ia");
+    img.style = "background:url('Img/" + lista5[i] + ".jpg')";
+    i = i + 1;
+    console.log(lista5 + " " + lista5[i]);
 }
 
-function rand(n) {
-    return (Math.floor(Math.random() * n + 5));
+function anterior() {
+    i < 0 ? (i = 4) : (i = i);
+    var img = document.getElementById("ia");
+    img.style = "background:url('Img/" + lista5[i] + ".jpg')";
+    i = i - 1;
+    console.log(lista5 + " " + lista5[i]);
 }
-var cambia_imagen = new Array();
 
-cambia_imagen[0] = "1.jpg";
-cambia_imagen[1] = "2.jpg";
-cambia_imagen[2] = "3.jpg";
-cambia_imagen[3] = "4.jpg";
-cambia_imagen[4] = "5.jpg";
-cambia_imagen[6] = "6.jpg";
-cambia_imagen[7] = "7.jpg";
-cambia_imagen[8] = "8.jpg";
-cambia_imagen[9] = "9.jpg";
-cambia_imagen[10] = "10.jpg";
-
-function cambiar() {
-    
-    document.getElementById("ia").src = cambia_imagen[rand(11)-1];
+function aletorio() {
+    var myArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+    var x, j, k;
+    for (x = myArray.length; x; x--) {
+        j = Math.floor(Math.random() * x);
+        k = myArray[x - 1];
+        myArray[x - 1] = myArray[j];
+        myArray[j] = k;
+    }
+    for (let index = 0; index < 5; index++) {
+        lista5[index] = myArray[index];
+    }
+    i = 1;
+    var img = (document.getElementById("ia").style =
+        "background:url('Img/" + lista5[0] + ".jpg')");
 }
